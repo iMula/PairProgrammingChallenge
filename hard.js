df047d87d91013b5636f360750b41b350dc786b9
Seth Mulia <smulia@r2hstudent.com>
2022-01-27 18:22:01 -0500

    completed

--- hard.js
+++ hard.js
@@ -1,48 +1,16 @@
 //Write a function that takes in a string and returns a boolean value whether or not the string contains a pair of matching brackets ({}, [], ()). These brackets must be nested appropriately in order to return a true value
 
 
+function brackets(str){
+ 
+  str = str.replace(/[^{}()[\]]+/g, "");
+  if(str === "{}" || str === "[]" || str === "()"){
+      return true;
+  }
+  else{
+      return false;
+  }
+}
 
-var isValid = function(s) {
-    const stack = [];
-    const characters = { ')': '(', '}': '{', ']': '['};
-  for (const char of s) {
-        
-      if (!characters[char]){
-         stack.push(char); 
-      } 
-      else if (stack.pop() !== characters[char]){
-          return false;
-      } 
-    }
-    return stack.length === 0;
-  };
-
-isValid();
-
-
-
-const isValid = function(s) {
-    const stack = [];
-    const closings = {
-      '(': ')',
-      '{': '}',
-      '[': ']'
-    };
-    
-    for (let i = 0; i < s.length; i += 1) {
-      if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
-        stack.push(closings[s[i]])
-        continue;
-      }
-      
-      const requiredElement = stack.pop();
-      
-      if (s[i] !== requiredElement) return false;
-    }
-    
-    if (stack.length > 0) return false;
-    
-    return true;
-  };
-
-  isValid();
+str = "[Hello .world]";
+console.log(brackets(str));
