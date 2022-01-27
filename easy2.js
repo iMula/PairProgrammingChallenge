e4008ce56e5817e19fb9d9a71ff870df8995412b
Seth Mulia <smulia@r2hstudent.com>
2022-01-27 17:34:03 -0500

    fiinshed hard challenge

--- easy2.js
+++ easy2.js
@@ -1,29 +1,5 @@
 //Create a function that checks an array for prime numbers then inserts any primes into a new array. 
 
- 
-
-// function checkPrime(numArray) {
-//     if (numArray <= 1) {
-//         return false
-//     }else {
-//         for (let i = 2; i < numArray; i++) {
-//             if (numArray % i == 0) {
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// }
-// const isPrime = checkPrime(2);
-// console.log(isPrime); // false
-
-// numArray.forEach(function (element) {
-//     const isPrime = checkPrime(element);
-//     if (isPrime) {
-//       console.log(`${element} is a prime number`);
-//     } else {
-//     }
-//   });
 
 numArray = [1, 2, 3, 4, 5, 6, 7, 8]
 const isPrime = (numArray) => {
